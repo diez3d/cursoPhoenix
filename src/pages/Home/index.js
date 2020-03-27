d3ff3521a2d295b67ed1de594320fbc25c2186f5
Jose Carlos <josec.limon@hotmail.com>
2020-03-27 10:33:35 +0100

    Change title

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -26,7 +26,7 @@ export default ({ history }) =>{
             <Card className={classes.cardContainer}>
                 <Grid container className={classes.titleGridContainer} >
                     <Grid>
-                        <Typography className={classes.title}>Bienvenido </Typography>
+                        <Typography className={classes.title}>Bienvenido Lucas </Typography>
                     </Grid>
                     <Grid>
                         <MovieIcon className={classes.movieIcon}></MovieIcon>
